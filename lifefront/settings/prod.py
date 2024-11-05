f2aa9dd18e6474a4153d31e2805e90f6e13e6b76
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2024-11-05 13:26:46 +0330

    fuck liara reverted everything

--- lifefront/settings/prod.py
+++ lifefront/settings/prod.py
@@ -3,9 +3,9 @@ from .common import *
 
 DEBUG = False
 
-SECRET_KEY = "fd9+if!o&4mdn5dixonjirs%t4453h_&%a26=t0j*v1sm_+2k_"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "https://elastic-aryabhata-qzkaaw7ph.liara.run/", "elastic-aryabhata-qzkaaw7ph.liara.run/"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0"]
 
 DATABASES = {
     'default': {
