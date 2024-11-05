94b1bf0515e05f8c5827c24f8329b61f791bc018
meGQD <m.r.k.27272727.m.r.k@gmail.com>
2024-11-05 12:55:20 +0330

    Allowed hosts added

--- lifefront/settings/prod.py
+++ lifefront/settings/prod.py
@@ -5,7 +5,7 @@ DEBUG = False
 
 SECRET_KEY = 'fd9+if!o&4mdn5dixonjirs%t4453h_&%a26=t0j*v1sm_+2k_'
 
-ALLOWED_HOSTS = ["localhost", '0.0.0.0']
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "https://elastic-aryabhata-qzkaaw7ph.liara.run/", "elastic-aryabhata-qzkaaw7ph.liara.run/"]
 
 DATABASES = {
     'default': {
